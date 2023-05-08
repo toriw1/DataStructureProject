62ea72a4c0a1fead64ce374ddda52d501b245475
Tori Williams <vwilliams@guilford.edu>
2023-05-08 13:07:50 -0400

    project 8

--- datastructureproject/src/main/java/edu/guilford/ReadTextFile.java
+++ datastructureproject/src/main/java/edu/guilford/ReadTextFile.java
@@ -4,55 +4,61 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.PriorityQueue;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class ReadTextFile {
     public static void main(String[] args) throws IOException {
         Path dataLocation = null; // what's our data file?
         String fileName = null;
-        
+
+        PriorityQueue<String> fileQueue = new PriorityQueue<String>();
+
+        Set<String> fileSet = new TreeSet<String>();
+
         // open the file and read the data
         try {
-            Path locationPath = Paths.get(ReadTextFile.class.getResource("/" + "The-Use-of-First-Person-Point-of-View-in-Jane-Eyre.txt").toURI());
+            Path locationPath = Paths.get(ReadTextFile.class
+                    .getResource("/" + "The-Use-of-First-Person-Point-of-View-in-Jane-Eyre.txt").toURI());
             // create a file reader
-            FileReader fileReader= new FileReader(locationPath.toFile());
+            // FileReader fileReader = new FileReader(locationPath.toFile());
             // create a buffered reader
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(locationPath.toFile()));
             // read the data from the file
             String line = bufferedReader.readLine();
-            // set up the output file
-            fileName = "output.txt";
-            dataLocation = Paths.get(fileName);
+            PrintWriter fileQueuePrint = new PrintWriter(new FileWriter("output_queue.txt"));
+            PrintWriter fileQueuePolled = new PrintWriter(new FileWriter("output_queue_polled.txt"));
             // while reading the line, split the line into words and add them to a queue
             // exclude punctuation, and make all words lowercase
             // let the natural order of the words be alphabetical
             while (line != null) {
                 // split the line into words
-                String[] words = line.split(" ");
+                String[] words = line.split("\\W+");
                 // for each word in the line
-                for (String word : words) {
-                    // remove punctuation
-                    word = word.replaceAll("[^a-zA-Z0-9]", "");
-                    // make the word lowercase
-                    word = word.toLowerCase();
-                    // add the word to the queue
-                    //queue.enqueue(word);
+                for (int i = 0; i < words.length; i++) {
+                    String word = words[i];
+                    if (!word.isEmpty()) {
+                        fileQueue.add(word.toLowerCase());
+                        fileSet.add(word.toLowerCase());
+                    }
                 }
-                // read the next line
-                line = bufferedReader.readLine();
             }
-             // create a file writer to output.txt
-             FileWriter fileWriter = new FileWriter(dataLocation.toFile());
-             // write the data to the file
-             fileWriter.write(line);
-             // close the file writer
-             fileWriter.close();
-            // close the file reader
-            fileReader.close();
-            // close the file
-            bufferedReader.close();
+                // print the queue to a file
+                while (!fileQueue.isEmpty()) {
+                    fileQueuePrint.println(fileQueue);
+                    fileQueuePolled.println(fileQueue.poll());
+                }
+                fileQueuePrint.close();
+                fileQueuePolled.close();
+                // print the set to a file
+                PrintWriter fileSetPrint = new PrintWriter(new FileWriter("output_set.txt"));
+                fileSetPrint.println(fileSet);
+                fileSetPrint.close();
         } catch (URISyntaxException e) {
             System.out.println("Error reading file: " + e.getMessage());
         }
